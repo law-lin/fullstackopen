25072f23f5ea2f1fcf649cc406895aa1c52acc9a
law-lin <lawrencelin101@gmail.com>
2020-11-26 03:47:40 -0500

    FinishedExercises 6.13-6.14

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { createAnecdote } from '../reducers/anecdoteReducer';
 import { createAnecdoteNotif } from '../reducers/notificationReducer';
+import anecdoteService from '../services/anecdotes';
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault();
     const anecdote = event.target.anecdote.value;
     event.target.anecdote.value = '';
-    dispatch(createAnecdote(anecdote));
-    dispatch(createAnecdoteNotif(anecdote));
+    const newAnecdote = await anecdoteService.createNew(anecdote);
+    dispatch(createAnecdote(newAnecdote.content));
+    dispatch(createAnecdoteNotif(newAnecdote.content));
   };
 
   return (
--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -6,7 +6,6 @@ import App from './App';
 import anecdoteReducer from './reducers/anecdoteReducer';
 import notificationReducer from './reducers/notificationReducer';
 import filterReducer from './reducers/filterReducer';
-
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const reducer = combineReducers({
